6d9af2cd31e493c5353b85fbc7c6d2e2bc7bbccf
rogerthatdev <roger.that.dev@gmail.com>
2023-06-07 10:01:04 -0700

    move terraform vars

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,3 @@
-variable "project_id" {
-    type = string
-    default = "cloud-run-sandbox-388917"
-}
-
 locals {
   services = [
     "cloudbuild.googleapis.com",
@@ -15,21 +10,21 @@ locals {
 
 
 resource "google_project_service" "required" {
-  project = var.project_id
+  project  = var.project_id
   for_each = toset(local.services)
-  service = each.value
+  service  = each.value
 }
 
 resource "google_secret_manager_secret" "nginx_config" {
   secret_id = "nginx-config"
-  project = var.project_id
+  project   = var.project_id
   replication {
     automatic = true
   }
 }
 
 data "local_file" "nginx_conifg" {
-    filename = "${path.module}/files/nginx_config"
+  filename = "${path.module}/files/nginx_config"
 }
 
 resource "google_secret_manager_secret_version" "nginx_config" {
@@ -41,14 +36,14 @@ resource "google_secret_manager_secret_version" "nginx_config" {
 
 # Secret manager secret accessor for service account
 data "google_compute_default_service_account" "default" {
-    project = var.project_id
+  project = var.project_id
 }
 
 
 resource "google_secret_manager_secret_iam_member" "member" {
-  project = var.project_id
+  project   = var.project_id
   secret_id = google_secret_manager_secret.nginx_config.secret_id
-  role = "roles/secretmanager.secretAccessor"
-  member = "serviceAccount:${data.google_compute_default_service_account.default.email}"
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${data.google_compute_default_service_account.default.email}"
 }
 
